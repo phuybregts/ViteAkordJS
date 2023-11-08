2d34c4ca8c47dd3ef30b132584303e7178d162f9
ph <phuybregts@gmail.com>
2023-11-08 10:55:43 +0100

    Included polyfills

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-// import { Akord } from "@akord/akord-js";
+import { Akord, Auth } from "@akord/akord-js";
 
 function App() {
   const [count, setCount] = useState(0);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ["@akord/akord-js"],
+  plugins: [
+    react(),
+    nodePolyfills({
+      exclude: ["fs"],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
+  define: {
+    "process.env": {},
   },
 });
